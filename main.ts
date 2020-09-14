47fcf2a8d30eab518415aa52bcf0596733d849e4
AWEman11 <69909177+AWEman11@users.noreply.github.com>
2020-09-14 09:56:26 -0400

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
+scene.setBackgroundColor(6)
 info.setLife(3)
-game.splash("Press A to Flap the Bird")
+game.splash("Press A to Boost Mario")
 //  Setup player
 let Mario = sprites.create(img`
     . . . . . . . . . . . . . . . .
@@ -22,18 +23,15 @@ let Mario = sprites.create(img`
 Mario.setKind(SpriteKind.Player)
 Mario.ay = 190
 Mario.x = 40
-//  Fly
 controller.A.onEvent(ControllerButtonEvent.Pressed, function flap() {
     Mario.setVelocity(0, -100)
 })
-//  Generate Columns
 game.onUpdateInterval(1500, function on_update_interval() {
     let gate_img = image.create(10, scene.screenHeight())
     gate_img.fill(7)
     gate_img.fillRect(0, randint(10, 40), 10, randint(50, 80), 0)
     let gate = sprites.createProjectileFromSide(gate_img, -50, 0)
     gate.setPosition(scene.screenWidth(), scene.screenHeight() / 2)
-    //  Increase score
     gate.onDestroyed(function on_on_destroyed() {
         info.changeScoreBy(1)
     })
@@ -41,7 +39,6 @@ game.onUpdateInterval(1500, function on_update_interval() {
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function on_overlap(sprite: Sprite, otherSprite: Sprite) {
     death()
 })
-//  Game Loop
 game.onUpdate(function on_update() {
     if (Mario.y > scene.screenHeight()) {
         death()
@@ -50,7 +47,6 @@ game.onUpdate(function on_update() {
     }
     
 })
-//  Flappy Dies
 function death() {
     info.changeLifeBy(-1)
     if (info.life() != 0) {
